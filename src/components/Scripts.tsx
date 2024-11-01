0213fb770ae3088247a05464d5a04c23cd5021d2
Max Meinders <max.meinders@gmail.com>
2024-11-01 08:48:32 +0100

    fix: remove meta.DEV from Scripts

--- src/components/Scripts.tsx
+++ src/components/Scripts.tsx
@@ -1,14 +1,8 @@
 import { useContext } from "react";
 import { assetsContext } from "./AssetsProvider.js";
-import { virtualClientEntry } from "../plugin.js";
 
 export function Scripts() {
   const assets = useContext(assetsContext);
-
-  // TODO: find out why assetsContext via ssrLoadModule does not work
-  if ((import.meta as any).env.DEV) {
-    return <script type="module" src={`/${virtualClientEntry}`}></script>;
-  }
 
   return (
     assets
